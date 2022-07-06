873932dd4ffbd5ad18a63f1c14961fd8bf277efd
Steve Boyd <emteknetnz@gmail.com>
2022-07-06 12:09:24 +1200

    .

--- status.php
+++ status.php
@@ -43,14 +43,39 @@ foreach ($ghrepos as $ghrepo) {
             break;
         }
     }
+    $default_branch = 'master';
+    echo "Fetching branch data for $ghrepo\n";
+    $ch = create_ch("https://api.github.com/repos/$ghrepo/branches");
+    $result = curl_exec($ch);
+    $resp_code = curl_getinfo($ch, CURLINFO_RESPONSE_CODE);
+    curl_close($ch);
+    if (!$result) {
+        echo "Could not fetch pull-requests for $ghrepo, exiting\n";
+        die;
+    }
+    $branches = [];
+    foreach (json_decode($result) as $br) {
+        $branch = $br->name;
+        if (!preg_match('#^[1-9]$#', $branch)) {
+            continue;
+        }
+        $branches[] = (int) $branch;
+    }
+    if (count($branches)) {
+        $default_branch = (string) max($branches);
+    }
     $ccs_repo = 'creative-commoners/' . explode('/', $ghrepo)[1];
-    $src = "https://github.com/$ccs_repo/actions/workflows/ci.yml/badge.svg?branch=$pr_branch";
-    $href = "https://github.com/$ccs_repo/actions?query=branch%3A$pr_branch";
+    $badge_src = "https://github.com/$ccs_repo/actions/workflows/ci.yml/badge.svg?branch=$pr_branch";
+    $actions_href = "https://github.com/$ccs_repo/actions?query=branch%3A$pr_branch";
+    $workflow_href = "https://github.com/$ghrepo/blob/$default_branch/.github/workflows/ci.yml";
+    $travis_href = "https://github.com/$ghrepo/blob/$default_branch/.travis.yml";
     $class = $class == 'even' ? 'odd' : 'even';
     $h[] = implode("\n", [
         "<tr class=\"$class\">",
             "<td><a href=\"$pr_link\" target=\"_blank\">$ghrepo</a></td>",
-            "<td><a href=\"$href\" target=\"_blank\"><img src=\"$src\"></a></td>",
+            "<td><a href=\"$actions_href\" target=\"_blank\"><img src=\"$badge_src\"></a></td>",
+            "<td><a href=\"$travis_href\" target=\"_blank\">.travis.yml</a></td>",
+            "<td><a href=\"$workflow_href\" target=\"_blank\">merged ci.yml</td>",
         '</tr>'
     ]);
 }
