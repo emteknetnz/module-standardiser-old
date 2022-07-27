0f1336850252e8226ab1f5e000a6c4d6186fff8d
Steve Boyd <emteknetnz@gmail.com>
2022-07-27 17:51:33 +1200

    .

--- run.php
+++ run.php
@@ -17,25 +17,18 @@ $pr_links = [];
 # sboyd tmp
 $exclude_ghrepos = [
     'fallback',
-    // prs already created
-    'silverstripe/comment-notifications',
-    'silverstripe/recipe-kitchen-sink',
-    'silverstripe/silverstripe-installer',
-    'silverstripe/silverstripe-framework',
-    'silverstripe/silverstripe-reports',
-    'silverstripe/silverstripe-tagfield',
-    'silverstripe/silverstripe-siteconfig',
-    'silverstripe/silverstripe-versioned',
-    'silverstripe/silverstripe-versioned-admin',
-    'silverstripe/sspak',
-    // ci not relevant
-    'silverstripe/recipe-testing', // no code
-    
 ];
-$min_i = 100;
-$max_i = 110;
+$min_i = 0;
+$max_i = 10;
+
+$parent_issue = 'https://github.com/silverstripe/silverstripe-framework/issues/10349';
+$pr_title = 'DEP CMS 5 dependencies';
+$pr_branch = 'pulls/*/major-deps';
 
 $pr_urls = [];
+
+$use_earliest_supported_minor = false; // otherwise use default branch
+$create_new_major = true;
 
 foreach ($ghrepos as $i => $ghrepo) {
     if ($i < $min_i || $i >= $max_i || in_array($ghrepo, $exclude_ghrepos)) {
@@ -45,7 +38,6 @@ foreach ($ghrepos as $i => $ghrepo) {
     $account = explode('/', $ghrepo)[0];
     $repo = explode('/', $ghrepo)[1];
     $dir = "modules/$repo";
-    $title = 'MNT Use GitHub Actions CI';
     # see if dir already exists
     if (file_exists($dir)) {
         echo "Directory for $ghrepo already exists, continuing\n";
@@ -62,7 +54,7 @@ foreach ($ghrepos as $i => $ghrepo) {
     }
     $pr_exists = false;
     foreach (json_decode($result) as $pr) {
-        if ($pr->title == $title) {
+        if ($pr->title == $pr_title) {
             $pr_exists = true;
             break;
         }
@@ -85,6 +77,10 @@ foreach ($ghrepos as $i => $ghrepo) {
         echo "On a non-default branch for $ghrepo, not risking doing git things\n";
         die;
     }
+    if ($create_new_major && !preg_match('#^[1-9][0-9]*$#', $current_branch)) {
+        echo "On a non integer branch for creating new major, nope\n";
+        die;
+    }
     # get the earliest supported minor branch e.g. 4.10, when 4.11 and 4 both exist
     $branches = cmd([
         "cd $dir",
@@ -100,38 +96,64 @@ foreach ($ghrepos as $i => $ghrepo) {
     });
     natsort($bs);
     $bs = array_reverse($bs);
-    $current_branch = $bs[1] ?? $bs[0] ?? $current_branch;
-    # create new branch from the minor branch
-    $new_branch = "pulls/$current_branch/module-standards";
+    if ($use_earliest_supported_minor) {
+        $current_branch = $bs[1] ?? $bs[0] ?? $current_branch;
+    } else {
+        // just use the default branch
+    }
+    # create new branch from the current branch
+    $new_branch = str_replace('*', $current_branch, $pr_branch);
     cmd([
         "cd $dir",
         "git checkout $current_branch",
         "git checkout -b $new_branch",
         "cd - > /dev/null"
     ]);
-    # update workflows
-    $path = "$dir/.github/workflows";
-    if (!file_exists($path)) {
-        mkdir($path, 0775, true);
+    if (true) {
+        // create new minors
+        $composer = json_decode(file_get_contents("$dir/composer.json"));
+        $require = $composer->require;
+        foreach ($require as $requirement => $version) {
+            // TODO: symbiote?
+            if (!preg_match('#^(silverstripe)/#', $requirement)) {
+                continue;
+            }
+            preg_match('#^\^[0-9]#', $version, $m);
+            $new_major = $m[1] + 1;
+            $require->{$requirement} = "^$new_major.0";
+        }
+        if (isset($require->{'silverstripe/recipe-cms'})) {
+            $require->{'silverstripe/recipe-cms'} = '^5.0';
+        } else {
+            $require->{'silverstripe/framework'} = '^5.0';
+        }
+        file_put_contents("$dir/composer.json", json_encode($composer));
     }
-    if (!file_exists("$path/ci.yml")) {
-        file_put_contents("$path/ci.yml", $creator->createWorkflow('ci', $ghrepo, ''));
+    if (false) {
+        # update workflows
+        $path = "$dir/.github/workflows";
+        if (!file_exists($path)) {
+            mkdir($path, 0775, true);
+        }
+        if (!file_exists("$path/ci.yml")) {
+            file_put_contents("$path/ci.yml", $creator->createWorkflow('ci', $ghrepo, ''));
+        }
+        if (!file_exists("$path/keepalive.yml")) {
+            file_put_contents("$path/keepalive.yml", $creator->createWorkflow('keepalive', $ghrepo, ''));
+        }
+        # update readme badges from travis to gha - it's assumed they are always present
+        $fn = file_exists("modules/$repo/README.md") ? "modules/$repo/README.md" : "modules/$repo/readme.md";
+        $readme = file_get_contents($fn);
+        $replace = "[![CI](https://github.com/$account/$repo/actions/workflows/ci.yml/badge.svg)](https://github.com/$account/$repo/actions/workflows/ci.yml)";
+        # branch defined
+        $find = preg_quote("[![Build Status](https://api.travis-ci.com/$account/$repo.svg?branch=)](https://travis-ci.com/$account/$repo)");
+        $find = str_replace('\\?branch\\=', '\\?branch\\=.+?', $find);
+        $readme = preg_replace("#$find#", $replace, $readme);
+        # branch not defined
+        $find = "[![Build Status](https://api.travis-ci.com/$account/$repo.svg)](https://travis-ci.com/$account/$repo)";
+        $readme = str_replace($find, $replace, $readme);
+        file_put_contents($fn, $readme);
     }
-    if (!file_exists("$path/keepalive.yml")) {
-        file_put_contents("$path/keepalive.yml", $creator->createWorkflow('keepalive', $ghrepo, ''));
-    }
-    # update readme badges from travis to gha - it's assumed they are always present
-    $fn = file_exists("modules/$repo/README.md") ? "modules/$repo/README.md" : "modules/$repo/readme.md";
-    $readme = file_get_contents($fn);
-    $replace = "[![CI](https://github.com/$account/$repo/actions/workflows/ci.yml/badge.svg)](https://github.com/$account/$repo/actions/workflows/ci.yml)";
-    # branch defined
-    $find = preg_quote("[![Build Status](https://api.travis-ci.com/$account/$repo.svg?branch=)](https://travis-ci.com/$account/$repo)");
-    $find = str_replace('\\?branch\\=', '\\?branch\\=.+?', $find);
-    $readme = preg_replace("#$find#", $replace, $readme);
-    # branch not defined
-    $find = "[![Build Status](https://api.travis-ci.com/$account/$repo.svg)](https://travis-ci.com/$account/$repo)";
-    $readme = str_replace($find, $replace, $readme);
-    file_put_contents($fn, $readme);
     # delete .travis
     // if (file_exists("modules/$repo/.travis.yml")) {
     //     unlink("modules/$repo/.travis.yml");
@@ -149,7 +171,7 @@ foreach ($ghrepos as $i => $ghrepo) {
     }
     cmd([
         "cd $dir",
-        "git commit -m '$title'",
+        "git commit -m '$pr_title'",
         "git remote add ccs git@github.com:creative-commoners/$repo",
         "git push ccs $new_branch",
         "cd - > /dev/null"
@@ -157,12 +179,11 @@ foreach ($ghrepos as $i => $ghrepo) {
     $url_new_branch = urlencode($new_branch);
     // https://docs.github.com/en/rest/pulls/pulls#create-a-pull-request
     $body = implode('<br /><br />', [
-        'Issue https://github.com/silverstripe/gha-ci/issues/11',
-        "Workflow run https://github.com/creative-commoners/$repo/actions?query=branch%3A$url_new_branch"
+        "Issue $parent_issue"
     ]);
     $post_body = <<<EOT
     {
-        "title": "$title",
+        "title": "$pr_title",
         "body": "$body",
         "head": "creative-commoners:$new_branch",
         "base": "$current_branch"
